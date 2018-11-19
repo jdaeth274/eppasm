4a2ea46cf3aa96388a352285a1f3723e07c72299
jdaeth274 <joshdaeth@yahoo.co.uk>
2018-11-19 20:03:13 +0000

    adding post_MRES_script

--- analysis/POST_MRES_RUNNING_SCRIPT.R
+++ analysis/POST_MRES_RUNNING_SCRIPT.R
@@ -2,9 +2,41 @@
 ## So this will be my R script for adapting the CSVAR model for Brazil data ###
 ## Lets try and keep this fairly tidy eh ######################################
 ###############################################################################
-
+require(didehpc)
 require(devtools)
 require(ggplot2)
+setwd("~/HOMES_drive/")
+options(didehpc.username = "jd2117",didehpc.home = "~/HOMES_drive/",
+        didehpc.cluster = "fi--didemrchnb", didehpc.credentials = "~/.smbcredentials")
+didehpc::didehpc_config(cores = 3,parallel = FALSE)
+didehpc::didehpc_config()
+devtools::load_all("~/Dropbox/jeff_hiv_work/eppasm")
+
+context::context_log_start()
+
+root <- "contexts_3"
+
+ctx<- context::context_save(root, packages = c("epp","eppasm","anclik","ggplot2","splines", "buildr"), 
+                            package_sources = provisionr::package_sources(local = c("~/Dropbox/jeff_hiv_work/eppasm",
+                                                                                    "~/Dropbox/MRes/hiv_project/epp",
+                                                                                    "~/Dropbox/MRes/hiv_project/anclik/anclik")))
+config <- didehpc::didehpc_config(cores = 1, parallel = FALSE)
+obj <- didehpc::queue_didehpc(ctx,config)
+a <- obj$enqueue(sessionInfo())
+a$wait(10)
+
+################################################################################
+## Only for when making changes to the eppasm folder ###########################
+################################################################################
+
+obj$provision(refresh_drat = TRUE)
+
+a <- obj$enqueue(sessionInfo())
+a$status()
+a$result()
+
+###############################################################################
+
 devtools::load_all("~/Dropbox/jeff_hiv_work/eppasm")
 load("~/Dropbox/jeff_hiv_work/r_data/brazil_spectrum_file", verbose = T)
 
@@ -12,3 +44,7 @@ brazil$fp$linear_diagnosis
 brazil$fp$neg_binom <- FALSE
 
 local_test_optim <- fitmod_csavr(brazil, incid_func ="ilogistic", B0 = 1e4, optfit = TRUE)
+
+test_optim_1_knot_neg <- obj$enqueue(fitmod_csavr(brazil, incid_func = "idbllogistic", B0 = 1e4, optfit = TRUE),
+                                     name = "double_log_knot_neg_diagn")
+  
